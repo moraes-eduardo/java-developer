3acce0e3dbe188cd9c371482a6d4bc2468edde6c
moraes-eduardo <moraes-eduardo@hotmail.com>
2023-01-19 19:41:00 -0300

    inclusao exemplos condicionais

---
+++ controle-fluxo/src/C4Ternaria.java
@@ -0,0 +1,9 @@
+public class C4Ternaria {
+    public static void main(String[] args) {
+        
+        int nota = 7;
+		String resultado = nota >=7 ? "Aprovado" : "Reprovado";
+		System.out.println(resultado);
+        
+    }
+}
